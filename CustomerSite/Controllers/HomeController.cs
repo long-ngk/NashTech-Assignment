b422e2fb6a07968b51455f20b4a345e76150a952
Long Nguyen <nguyenkimlog59@gmail.com>
2022-02-24 17:57:09 +0700

    Binding data from database to home page to display some products

--- CustomerSite/Controllers/HomeController.cs
+++ CustomerSite/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using General.Models;
+﻿using General.DataAccess;
+using General.Models;
+using General.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +9,24 @@ namespace CustomerSite.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ApplicationDbContext _db;
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Home()
         {
-            return View();
+            List<VM_Product> products = _db.Products.ToList()
+               .GroupJoin(_db.ProductImages.ToList(),
+               p => p.ProductID,
+               img => img.Product.ProductID,
+               (p, img) => new VM_Product { Product = p, ProductImages = img.ToList() })
+               .OrderByDescending(p => p.Product.Views)
+               .Take(6).ToList();
+
+            return View(products);
         }
 
         public IActionResult Privacy()
---
+++ General.Models/ViewModels/VM_Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace General.Models.ViewModels
+{
+    public class VM_Product
+    {
+        public Product Product { get; set; }
+        public List<ProductImage> ProductImages { get; set; }
+    }
+}
