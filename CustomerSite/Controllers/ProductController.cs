4b28b84c6e420e2c4fdc6a383657d91ec3e251e2
Long Nguyen <nguyenkimlog59@gmail.com>
2022-02-24 14:12:54 +0700

    Add product controller and product detail view

---
+++ CustomerSite/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerSite.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
