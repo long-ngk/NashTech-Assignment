e5f3221205e17e434d00c41925ec7b5763820d0a
Long Nguyen <nguyenkimlog59@gmail.com>
2022-03-06 14:51:55 +0700

    Add Dtos for product

---
+++ General.Models/Dtos/ProductDto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace General.Models.Dtos
+{
+    public class ProductDto
+    {
+        public int ProductID { get; set; }
+        [Required]
+        public string ProdName { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string SKU { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        [Required]
+        public DateTime UpdatedDate { get; set; } = DateTime.Now;
+        [Required]
+        public bool IsActive { get; set; }
+        [Required]
+        public int Views { get; set; }
+
+        public ProductCategory ProductCategory { get; set; }
+        public ProductDiscount ProductDiscount { get; set; }
+        public ICollection<ProductInventory> ProductInventories { get; set; }
+        public ICollection<CartItem> CartItems { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; }
+        public ICollection<ProductImage> ProductImages { get; set; }
+    }
+}
---
+++ General.Models/Dtos/ProductImageDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace General.Models.Dtos
+{
+    public class ProductImageDto
+    {
+        public int ProductImageID { get; set; }
+        public string ImageLink { get; set; }
+        public Product Product { get; set; }
+    }
+}
