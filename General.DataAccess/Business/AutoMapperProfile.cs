515a1731c3aa0386a6f5364aaaeb715144c759f7
Long Nguyen <nguyenkimlog59@gmail.com>
2022-03-06 14:49:48 +0700

    Create AutoMapperProfile to map from Model to Dto and vice versa Delete unnecessary file UnitOfWork and IUnitOfWork

---
+++ General.DataAccess/Business/AutoMapperProfile.cs
@@ -0,0 +1,33 @@
+﻿using General.Models;
+using General.Models.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace General.DataAccess.Business
+{
+    public class AutoMapperProfile : AutoMapper.Profile
+    {
+        public AutoMapperProfile()
+        {
+            FromDataAccessorLayer();
+            FromPresentationLayer();
+        }
+
+        private void FromPresentationLayer()
+        {
+            CreateMap<ProductDto, Product>();
+            CreateMap<ProductCategoryDto, ProductCategory>();
+            CreateMap<ProductImageDto, ProductImage>();
+        }
+
+        private void FromDataAccessorLayer()
+        {
+            CreateMap<Product, ProductDto>();
+            CreateMap<ProductCategory, ProductCategoryDto>();
+            CreateMap<ProductImage, ProductImageDto>();
+        }
+    }
+}
---
+++ General.DataAccess/Business/ServiceRegister.cs
@@ -0,0 +1,26 @@
+﻿using General.DataAccess.Business.Interfaces;
+using General.DataAccess.Business.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace General.DataAccess.Business
+{
+    public static class ServiceRegister
+    {
+        public static void AddBusinessLayer(this IServiceCollection services)
+        {
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+            services.AddTransient<IProductCategoryService, ProductCategoryService>();
+            services.AddTransient<IProductService, ProductService>();
+            services.AddTransient<IProductImageService, ProductImageService>();
+        }
+    }
+}
