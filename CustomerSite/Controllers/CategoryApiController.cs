716d151c665dc45ce837b55a211d6413ffb6ba47
Long Nguyen <nguyenkimlog59@gmail.com>
2022-03-15 16:08:05 +0700

    Add category api controller to get data in admin site (code with ReactJS)

---
+++ CustomerSite/Controllers/CategoryApiController.cs
@@ -0,0 +1,57 @@
+﻿using EnsureThat;
+using General.DataAccess.Business.Constants;
+using General.DataAccess.Business.Interfaces;
+using General.Models.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerSite.Controllers
+{
+    [Route("api/category")]
+    public class CategoryApiController : Controller
+    {
+        private readonly IProductCategoryService _productCategoryService;
+        public CategoryApiController(IProductCategoryService productCategoryService)
+        {
+            _productCategoryService = productCategoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCategory()
+        {
+            var _categories = await _productCategoryService.GetAllAsync();
+            return Ok(_categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCategoryById(int id)
+        {
+            var _categories = await _productCategoryService.GetByIdAsync(id);
+            return Ok(_categories);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromBody] ProductCategoryDto productCategoryDto)
+        {
+            Ensure.Any.IsNotNull(productCategoryDto, nameof(productCategoryDto));
+            await _productCategoryService.AddAsync(productCategoryDto);
+            return Created(Endpoints.Category, productCategoryDto);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync([FromBody] ProductCategoryDto productCategoryDto)
+        {
+            Ensure.Any.IsNotNull(productCategoryDto, nameof(productCategoryDto));
+            await _productCategoryService.UpdateAsync(productCategoryDto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync([FromRoute] int id)
+        {
+            var categoryDto = await _productCategoryService.GetByIdAsync(id);
+            Ensure.Any.IsNotNull(categoryDto, nameof(categoryDto));
+            await _productCategoryService.RemoveAsync(categoryDto);
+            return NoContent();
+        }
+    }
+}
---
+++ General.DataAccess/Business/Constants/Endpoints.cs
@@ -0,0 +1,7 @@
+﻿namespace General.DataAccess.Business.Constants
+{
+    public static class Endpoints
+    {
+        public const string Category = "api/category";
+    }
+}
